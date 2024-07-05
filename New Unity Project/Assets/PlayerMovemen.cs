13be32d2942020a169093d008b9c0bb1a3908ae5
Heyun Cai <caiheyuncoco@outlook.com>
2024-07-04 20:13:42 -0400

    E03 finish if statement

--- New Unity Project/Assets/PlayerMovemen.cs
+++ New Unity Project/Assets/PlayerMovemen.cs
@@ -5,6 +5,8 @@ public class PlayerMovemen : MonoBehaviour
 
 
     public Rigidbody rb;//toggle the rigitbody here
+    public float forwardForce = 2000f; // f means that I am  only talking about float
+    public float sidewaysForce = 500f;
 
     // Start is called before the first frame update
     void Start(){//run when the game starts, there can be many spaces
@@ -23,7 +25,18 @@ public class PlayerMovemen : MonoBehaviour
     // FixedUpdate us better to caluclate physics
     void FixedUpdate() {
 
-        rb.AddForce(0,0,2000 * Time.deltaTime);//same in all system
+        //Add a forward force
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);//same in all system
+
+        if (Input.GetKey("d")) //input use update so it may untoned with fixUpdate sometimes -> not worry now
+        {
+            rb.AddForce(sidewaysForce*Time.deltaTime,0,0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce*Time.deltaTime,0,0);
+        }
         
     }
 }
